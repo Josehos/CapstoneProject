6f13ab0b81fb018bb7705c04dc9326aa717f6a19
Josehos <jose.a.hoskins@gmail.com>
2022-12-20 16:17:31 -0700

    Merge #6 on 12/20/2022 with Lambda classes

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/LambdaService.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/LambdaService.java
@@ -2,6 +2,7 @@ package com.kenzie.capstone.service;
 
 import com.kenzie.capstone.service.dao.IngredientsDao;
 import com.kenzie.capstone.service.dao.IntoleranceDao;
+import com.kenzie.capstone.service.dao.RecipeDao;
 import com.kenzie.capstone.service.model.IngredientsData;
 import com.kenzie.capstone.service.model.IngredientsRecord;
 import com.kenzie.capstone.service.model.IntoleranceData;
@@ -18,35 +19,37 @@ public class LambdaService {
 
     private IngredientsDao ingredientsDao;
     private IntoleranceDao intoleranceDao;
+    private RecipeDao recipeDao;
 
 
     @Inject
-    public LambdaService(IngredientsDao ingredientsDao, IntoleranceDao intoleranceDao) {
+    public LambdaService(RecipeDao recipeDao) {
         this.ingredientsDao = ingredientsDao;
         this.intoleranceDao = intoleranceDao;
+        this.recipeDao = recipeDao;
     }
 
-    public IngredientsData getIngredientsData(String id) {
-        List<IngredientsRecord> records = ingredientsDao.getIngredientsData(id);
-        if (records.size() > 0) {
-            return new IngredientsData(records.get(0).getId(), records.get(0).getData());
-        }
-        return null;
-    }
-
-    public IngredientsData setIngredientsData(String data) {
-        String id = UUID.randomUUID().toString();
-        IngredientsRecord record = ingredientsDao.setIngredientsData(id, data);
-        return new IngredientsData(id, data);
-    }
-
-    public IntoleranceData getIntoleranceData(String id) {
-        List<IntoleranceRecord> records = intoleranceDao.getIntoleranceData(id);
-        if (records.size() > 0) {
-            return new IntoleranceData(records.get(0).getId(), records.get(0).getData());
-        }
-        return null;
-    }
+//    public IngredientsData getIngredientsData(String id) {
+//        List<IngredientsRecord> records = ingredientsDao.getIngredientsData(id);
+//        if (records.size() > 0) {
+//            return new IngredientsData(records.get(0).getId(), records.get(0).getData());
+//        }
+//        return null;
+//    }
+//
+//    public IngredientsData setIngredientsData(String data) {
+//        String id = UUID.randomUUID().toString();
+//        IngredientsRecord record = ingredientsDao.setIngredientsData(id, data);
+//        return new IngredientsData(id, data);
+//    }
+//
+//    public IntoleranceData getIntoleranceData(String id) {
+//        List<IntoleranceRecord> records = intoleranceDao.getIntoleranceData(id);
+//        if (records.size() > 0) {
+//            return new IntoleranceData(records.get(0).getId(), records.get(0).getData());
+//        }
+//        return null;
+//    }
 
     public IntoleranceData setIntoleranceData(String data) {
         String id = UUID.randomUUID().toString();
--- ServiceLambda/src/main/java/com/kenzie/capstone/service/dao/RecipeDao.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/dao/RecipeDao.java
@@ -2,16 +2,61 @@ package com.kenzie.capstone.service.dao;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class RecipeDao {
     private static final String URL = "https://api.spoonacular.com/recipes/complexSearch?";
+    private static final String API_KEY = "&apiKey=ac20e561ee6c4d8db924b44a0b3db6db";
+    private static final String INTOLERANCE = "intolerance=";
+    private static final String INGREDIENTS = "includeIngredients=";
 
     public String getRecipesByRestrictions(String dietaryRestriction) {
+        HttpClient client = HttpClient.newHttpClient();
 
-        return "";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL.concat(INTOLERANCE + dietaryRestriction).concat(API_KEY)))
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = httpResponse.statusCode();
+            if (statusCode == 200) {
+                return httpResponse.body();
+            } else {
+                return String.format("GET request failed: %d status code received", statusCode);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            return e.getMessage();
+        }
     }
 
     public String getRecipesByIngredients(String ingredients) {
+        HttpClient client = HttpClient.newHttpClient();
 
-        return "";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL.concat(INGREDIENTS + ingredients).concat(API_KEY)))
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = httpResponse.statusCode();
+            if (statusCode == 200) {
+                return httpResponse.body();
+            } else {
+                return String.format("GET request failed: %d status code received", statusCode);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            return e.getMessage();
+        }
     }
 }
