d86d4394bfd04316ddea0807a55122887ddc38b2
SamanthaJuaire <samantha.juaire@gmail.com>
2023-01-03 13:51:30 -0600

    Commented out Example classes to allow project to build

--- IntegrationTests/src/test/java/com/kenzie/appserver/controller/UserControllerTest.java
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/UserControllerTest.java
@@ -2,7 +2,6 @@ package com.kenzie.appserver.controller;
 
 import com.kenzie.appserver.IntegrationTest;
 import com.kenzie.appserver.controller.model.UserCreateRequest;
-import com.kenzie.appserver.service.ExampleService;
 import com.kenzie.appserver.service.model.Example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,50 +19,50 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@IntegrationTest
-class UserControllerTest {
-    @Autowired
-    private MockMvc mvc;
-
-    @Autowired
-    ExampleService exampleService;
-
-    private final MockNeat mockNeat = MockNeat.threadLocal();
-
-    private final ObjectMapper mapper = new ObjectMapper();
-
-    @Test
-    public void getById_Exists() throws Exception {
-
-        String name = mockNeat.strings().valStr();
-
-        Example persistedExample = exampleService.addNewExample(name);
-        mvc.perform(get("/example/{id}", persistedExample.getId())
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("id")
-                        .isString())
-                .andExpect(jsonPath("name")
-                        .value(is(name)))
-                .andExpect(status().is2xxSuccessful());
-    }
-
-    @Test
-    public void createExample_CreateSuccessful() throws Exception {
-        String name = mockNeat.strings().valStr();
-
-        UserCreateRequest userCreateRequest = new UserCreateRequest();
-        userCreateRequest.setName(name);
-
-        mapper.registerModule(new JavaTimeModule());
-
-        mvc.perform(post("/example")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(userCreateRequest)))
-                .andExpect(jsonPath("id")
-                        .exists())
-                .andExpect(jsonPath("name")
-                        .value(is(name)))
-                .andExpect(status().is2xxSuccessful());
-    }
-}
+//@IntegrationTest
+//class UserControllerTest {
+//    @Autowired
+//    private MockMvc mvc;
+//
+//    @Autowired
+//    ExampleService exampleService;
+//
+//    private final MockNeat mockNeat = MockNeat.threadLocal();
+//
+//    private final ObjectMapper mapper = new ObjectMapper();
+//
+//    @Test
+//    public void getById_Exists() throws Exception {
+//
+//        String name = mockNeat.strings().valStr();
+//
+//        Example persistedExample = exampleService.addNewExample(name);
+//        mvc.perform(get("/example/{id}", persistedExample.getId())
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(jsonPath("id")
+//                        .isString())
+//                .andExpect(jsonPath("name")
+//                        .value(is(name)))
+//                .andExpect(status().is2xxSuccessful());
+//    }
+//
+//    @Test
+//    public void createExample_CreateSuccessful() throws Exception {
+//        String name = mockNeat.strings().valStr();
+//
+//        UserCreateRequest userCreateRequest = new UserCreateRequest();
+//        userCreateRequest.setName(name);
+//
+//        mapper.registerModule(new JavaTimeModule());
+//
+//        mvc.perform(post("/example")
+//                        .accept(MediaType.APPLICATION_JSON)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(mapper.writeValueAsString(userCreateRequest)))
+//                .andExpect(jsonPath("id")
+//                        .exists())
+//                .andExpect(jsonPath("name")
+//                        .value(is(name)))
+//                .andExpect(status().is2xxSuccessful());
+//    }
+//}
--- ServiceLambdaJavaClient/src/main/java/com/kenzie/capstone/service/client/LambdaServiceClient.java
+++ ServiceLambdaJavaClient/src/main/java/com/kenzie/capstone/service/client/LambdaServiceClient.java
@@ -9,8 +9,8 @@ public class LambdaServiceClient {
     private static final String SET_EXAMPLE_ENDPOINT = "example";
 
 
-    private final String DIETARY_RESTRICTIONS = "?intolerances=";
-    private final String INGREDIENTS = "?includeIngredients=";
+//    private final String DIETARY_RESTRICTIONS = "?intolerances=";
+//    private final String INGREDIENTS = "?includeIngredients=";
 
     private final String DIETARY_RESTRICTIONS = "/intolerance/{intolerances}";
     private final String INGREDIENTS = "/ingredients/{includeIngredients}";
@@ -22,29 +22,29 @@ public class LambdaServiceClient {
         this.mapper = new ObjectMapper();
     }
 
-    public ExampleData getExampleData(String id) {
-        EndpointUtility endpointUtility = new EndpointUtility();
-        String response = endpointUtility.getEndpoint(GET_EXAMPLE_ENDPOINT.replace("{id}", id));
-        ExampleData exampleData;
-        try {
-            exampleData = mapper.readValue(response, ExampleData.class);
-        } catch (Exception e) {
-            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
-        }
-        return exampleData;
-    }
-
-    public ExampleData setExampleData(String data) {
-        EndpointUtility endpointUtility = new EndpointUtility();
-        String response = endpointUtility.postEndpoint(SET_EXAMPLE_ENDPOINT, data);
-        ExampleData exampleData;
-        try {
-            exampleData = mapper.readValue(response, ExampleData.class);
-        } catch (Exception e) {
-            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
-        }
-        return exampleData;
-    }
+//    public ExampleData getExampleData(String id) {
+//        EndpointUtility endpointUtility = new EndpointUtility();
+//        String response = endpointUtility.getEndpoint(GET_EXAMPLE_ENDPOINT.replace("{id}", id));
+//        ExampleData exampleData;
+//        try {
+//            exampleData = mapper.readValue(response, ExampleData.class);
+//        } catch (Exception e) {
+//            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+//        }
+//        return exampleData;
+//    }
+//
+//    public ExampleData setExampleData(String data) {
+//        EndpointUtility endpointUtility = new EndpointUtility();
+//        String response = endpointUtility.postEndpoint(SET_EXAMPLE_ENDPOINT, data);
+//        ExampleData exampleData;
+//        try {
+//            exampleData = mapper.readValue(response, ExampleData.class);
+//        } catch (Exception e) {
+//            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+//        }
+//        return exampleData;
+//    }
 
 
     public String getRecipesByRestrictions(String intolerance) {
