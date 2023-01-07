7cfb0cbab9249c1d96caa52927961676afd50bc3
SamanthaJuaire <samantha.juaire@gmail.com>
2023-01-07 09:17:04 -0600

    Added back @Service annotation on UserProfile.java that somehow got deleted

--- Application/src/main/java/com/kenzie/appserver/service/model/UserProfile.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/UserProfile.java
@@ -1,8 +1,11 @@
 package com.kenzie.appserver.service.model;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class UserProfile {
 
     private String username;
